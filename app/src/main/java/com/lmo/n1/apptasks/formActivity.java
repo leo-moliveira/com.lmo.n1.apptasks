b1ed81e35fbbe24489f0dcc086e26e521920a170
Leonardo Machado de Oliveira <leo.oliveira8@gmail.com>
2021-05-11 17:57:54 -0300

    project structure

---
+++ app/src/main/java/com/lmo/n1/apptasks/formActivity.java
@@ -0,0 +1,14 @@
+package com.lmo.n1.apptasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
